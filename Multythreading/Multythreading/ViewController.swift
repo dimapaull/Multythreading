eecef2f2ee0d170118eb4630cfa82f3b6af23ccd
dimapaull <dimapaull@yandex.ru>
2024-03-19 00:27:06 +0300

    выполнена таска

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Thread.detachNewThread {
+            for _ in (0..<10) {
+                let currentThread = Thread.current
+                print("1, Current thread: \(currentThread)")
+            }
+        }
+        
+        
+        for _ in (0..<10) {
+            let currentThread = Thread.current
+            print("2, Current thread: \(currentThread)")
+        }
     }
+    
+    /// Только первый цикл перевести в другой поток с помощью Thread.detachNewThread и обяснить почему изменился вывод.
+    /* Цикл теперь выполняется не один за другим, а в разнобой.
+     Это явный признак того, что задачи выполняются одновременно.
+     После выполнения блока кода поток будет закрыт.
+     Это объясняется тем, что у нас Thread.detachNewThread
+     Это выполение блока замыкания на другом потоке.
+     Минус этого подхода в том, что управлять этим новым потоком вы не сможете.*/
 }
 
