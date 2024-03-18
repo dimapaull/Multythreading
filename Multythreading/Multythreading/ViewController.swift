9769e4d4d6e27bb3a9f5c0279d871d3b8481c9ea
dimapaull <dimapaull@yandex.ru>
2024-03-19 00:10:07 +0300

    сделан шаблон

---
+++ Multythreading/Multythreading/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Multythreading
+//
+//  Created by Dmitry Pavlov on 18.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
