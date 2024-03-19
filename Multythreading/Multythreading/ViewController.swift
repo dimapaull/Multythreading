cda018b0a58a5cd8a39c5b7579429be9b7e16a4f
dimapaull <dimapaull@yandex.ru>
2024-03-20 01:23:32 +0300

    выполнено задание

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Выведется А С В Так как при запуске выполнятся сначала все задачи для этой очереди,
+    // а потом уже другие очереди, которые накинуты на текущую
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("A")
+        
+        DispatchQueue.main.async {
+            print("B")
+        }
+        
+        print("C")
     }
+    
 }
 
