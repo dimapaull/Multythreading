8d2100b105b154841bfb32ba49bc84eaf49c6b3e
dimapaull <dimapaull@yandex.ru>
2024-03-20 02:01:33 +0300

    сделана таска

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Сдесь проблеблема состояния гонки  время или порядок событий влияют на корректность результата.
+    
+    private lazy var name = "I love RM"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateName()
+    }
+    
+    func updateName() {
+        DispatchQueue.global().sync { // из асинк сделан синк
+            print(self.name)
+            print(Thread.current)
+        }
+        
+        print(self.name)
     }
 }
 
