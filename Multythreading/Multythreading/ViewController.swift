d73d96b5e784c1178b9a5fc3dabe40d86cf91d45
dimapaull <dimapaull@yandex.ru>
2024-03-20 01:46:14 +0300

    сделана таска

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -1,16 +1,28 @@
-//
-//  ViewController.swift
-//  Multythreading
-//
-//  Created by Dmitry Pavlov on 18.03.24.
-//
-
+import PlaygroundSupport
 import UIKit
 
-class ViewController: UIViewController {
 
+// Выведется 1 7 я написал. На самом деле вывелось 1 2 6 7 3.
+// Потосу что вью дид лоэд сработал из за инициализации
+// viewDidLoad() Он вызвался так как вью контроллер был весь создан и готов поместиться в адрес
+// в памяти(super.viewDidLoad)
+
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(2)
+        DispatchQueue.main.async {
+            print(3)
+            DispatchQueue.main.async { // сделана асинхронная очередь, потому что нельзя на текущей вызывать синхронную
+                print(5)
+            }
+            print(4)
+        }
+        print(6)
     }
 }
 
+let vc = ViewController()
+print(1)
+let view = vc.view
+print(7)
