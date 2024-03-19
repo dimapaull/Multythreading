f62a5c469bd1df3a5fe010b920a559073a7a4478
dimapaull <dimapaull@yandex.ru>
2024-03-19 15:36:09 +0300

    выполнена задача

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -8,9 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Создаем и запускаем поток
+        let infinityLoop = InfinityLoop()
+        infinityLoop.start()
+        print(infinityLoop.isFinished)
+        // Подождем некоторое время, а затем отменяем выполнение потока
+        sleep(5)
+        // Отменяем тут
+        infinityLoop.cancel()
+        print(infinityLoop.isFinished)
+        print(infinityLoop.isExecuting)
     }
 }
 
+class InfinityLoop: Thread {
+    var counter = 0
+    
+    override func main() {
+        while counter <= 5 && !isCancelled {
+            counter += 1
+            print(counter)
+            InfinityLoop.sleep(forTimeInterval: 1)
+        }
+    }
+    
+    
+    
+}
