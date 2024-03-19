64f62483236d2024006c705534aa8180ed309c82
dimapaull <dimapaull@yandex.ru>
2024-03-19 15:45:27 +0300

    выполнена задача

--- Multythreading/Multythreading/ViewController.swift
+++ Multythreading/Multythreading/ViewController.swift
@@ -11,6 +11,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let demonThread = ThreadDemon()
+        let angelThread = ThreadAngel()
+        
+        // 1
+        // demonThread.qualityOfService = .userInitiated
+        // angelThread.qualityOfService = .background
+        
+        // 2
+        // demonThread.qualityOfService = .background
+        // angelThread.qualityOfService = .userInitiated
+        
+        // 3
+        demonThread.qualityOfService = .userInitiated
+        angelThread.qualityOfService = .userInitiated
+        
+        demonThread.start()
+        angelThread.start()
+        
     }
 }
 
+
+class ThreadDemon: Thread {
+    override func main() {
+        for _ in 0..<100 {
+            print("1")
+        }
+    }
+}
+
+class ThreadAngel: Thread {
+    override func main() {
+        for _ in 0..<100 {
+            print("2")
+        }
+    }
+}
